e484d0567000121d2ee7f2e5bb2b1fc7f70b7d77
Takosineee <es955126@gmail.com>
2023-08-24 01:00:03 +0800

    addtocart

--- slnjCatChaOnlineShop/prjCatChaOnlineShop/Models/CModels/CCartItem.cs
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Models/CModels/CCartItem.cs
@@ -2,10 +2,12 @@
 {
     public class CCartItem
     {
+        //用來取得同名商品的attribute
+        public ShopProductTotal product { get;  }
         public int cId { get; set; }
         public string? cName { get; set; }
         public int? c剩餘庫存;
-        internal decimal? cPrice;
+        public decimal? cPrice { get; set; }
 
         public string? c子選項 { get; set; }
         public string? cImgPath { get; set; }
