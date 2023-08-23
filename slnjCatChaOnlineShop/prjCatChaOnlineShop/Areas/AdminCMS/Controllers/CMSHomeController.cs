163527e77286f6759ab981893001383a7707b850
MANDY LIANG <liang930517@yahoo.com.tw>
2023-08-23 14:02:05 +0800

    重新放入檔案和程式碼

--- slnjCatChaOnlineShop/prjCatChaOnlineShop/Areas/AdminCMS/Controllers/CMSHomeController.cs
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Areas/AdminCMS/Controllers/CMSHomeController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjCatChaOnlineShop.Areas.AdminCMS.Models;
+using prjCatChaOnlineShop.Areas.AdminCMS.Models.ViewModels;
+using prjCatChaOnlineShop.Models;
+using prjCatChaOnlineShop.Models.CDictionary;
+using System.Text.Json;
 
 namespace prjCatChaOnlineShop.Areas.AdminCMS.Controllers
 {
@@ -7,6 +12,25 @@ namespace prjCatChaOnlineShop.Areas.AdminCMS.Controllers
     {
         public IActionResult Login()
         {
+            //判斷是否有登入
+            if (HttpContext.Session.Keys.Contains(CAdminLogin.SK_LOGINED_USER))
+            {
+                return RedirectToAction("Member", "Member");
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(CAdminLoginVM vm)
+        {
+            ShopGameAdminData user = (new cachaContext()).ShopGameAdminData.FirstOrDefault(
+                t => t.AdminAccount.Equals(vm.AdminAccount) && t.AdminPassword.Equals(vm.AdminPassword));
+            if (user != null && user.AdminPassword.Equals(vm.AdminPassword))
+            {
+                string Json = JsonSerializer.Serialize(user);
+                HttpContext.Session.SetString(CAdminLogin.SK_LOGINED_USER, Json);
+                return RedirectToAction("Member", "Member");
+            }
+            //登錄失敗返回登入頁
             return View();
         }
     }
---
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Areas/AdminCMS/Models/CBannerWrap.cs
@@ -0,0 +1,27 @@
+﻿using prjCatChaOnlineShop.Models;
+using System.ComponentModel;
+
+namespace prjCatChaOnlineShop.Areas.AdminCMS.Models
+{
+    public class CBannerWrap
+    {
+        private GameShopBanner _banr = null;
+        public GameShopBanner banner { get { return _banr; } set { _banr = value; } }
+        public CBannerWrap() 
+        {
+            _banr = new GameShopBanner();
+        }
+
+        [DisplayName("ID")]
+        public int BannerId { get { return _banr.BannerId; } set { _banr.BannerId = value; } }
+
+        [DisplayName("名稱")]
+        public string Banner { get { return _banr.Banner; } set { _banr.Banner = value; } }
+
+        [DisplayName("發佈日期")]
+        public DateTime? PublishDate { get { return _banr.PublishDate; } set { _banr.PublishDate = value; } }
+
+        [DisplayName("圖片")]
+        public string Link { get { return _banr.Link; } set { _banr.Link = value; } }
+    }
+}
