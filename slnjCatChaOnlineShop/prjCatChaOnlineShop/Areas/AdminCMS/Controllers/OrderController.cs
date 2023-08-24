34b0b7860de2468f2042db3ad3c94200fc41eca6
mandy <liang930517@yahoo.com.tw>
2023-08-24 12:16:19 +0800

    訂單詳細資料第一版

--- slnjCatChaOnlineShop/prjCatChaOnlineShop/Areas/AdminCMS/Controllers/OrderController.cs
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Areas/AdminCMS/Controllers/OrderController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using prjCatChaOnlineShop.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace prjCatChaOnlineShop.Controllers.CMS
 {
@@ -49,6 +50,72 @@ namespace prjCatChaOnlineShop.Controllers.CMS
              ).ToList();
 
             return Json(new { data });
+        }
+
+        public IActionResult GetOrderDetails(int memberId, int orderId)
+        {
+            var memberInfo = _context.ShopMemberInfo
+            .FirstOrDefault(m => m.MemberId == memberId);
+
+            var orderTotal = _context.ShopOrderTotalTable
+                .FirstOrDefault(o => o.OrderId == orderId);
+
+            var orderDetails = _context.ShopOrderDetailTable
+                .Where(od => od.OrderId == orderId)
+                .ToList();
+
+            var productIds = orderDetails.Select(od => od.ProductId).ToList();
+
+            var products = _context.ShopProductTotal
+                .Where(p => productIds.Contains(p.ProductId))
+                .ToList();
+
+            var paymentMethod = _context.ShopPaymentMethodData
+                .FirstOrDefault(pm => pm.PaymentMethodId == orderTotal.PaymentMethodId)?.PaymentMethodName;
+
+            var shippingMethod = _context.ShopShippingMethod
+                .FirstOrDefault(sm => sm.ShippingMethodId == orderTotal.ShippingMethodId)?.ShippingMethodName;
+
+            var couponContent = _context.ShopCouponTotal
+                .FirstOrDefault(ct => ct.CouponId == orderTotal.CouponId)?.CouponContent;
+
+            var orderStatus = _context.ShopOrderStatusData
+                .FirstOrDefault(os => os.OrderStatusId == orderTotal.OrderStatusId)?.StatusName;
+
+            var data = new
+            {
+                MemberInfo = memberInfo,
+                OrderTotal = orderTotal,
+                OrderDetails = orderDetails,
+                Products = products,
+                PaymentMethod = paymentMethod,
+                ShippingMethod = shippingMethod,
+                CouponContent = couponContent,
+                OrderStatus = orderStatus
+            };
+
+            return Json( new { data });
+            //var order = _context.ShopMemberInfo
+            //          .Include(m => m.ShopOrderTotalTable)
+            //          .Include(m => m.ShopMemberCouponData)
+            //          .FirstOrDefault(m => m.MemberId == id);
+
+            //if (member != null)
+            //{
+            //    foreach (var couponData in member.ShopMemberCouponData)
+            //    {
+            //        // 根據 couponData.CouponId 查詢其他相關數據並附加到 couponData
+            //        var CouponTotal = _context.ShopCouponTotal.FirstOrDefault(o => o.CouponId == couponData.CouponId);
+            //    }
+
+            //    return Json(new { data = member });
+            //}
+            //else
+            //{
+            //    // 沒有找到匹配的成員
+            //    //  do something..........
+            //    return NotFound();
+            //}
         }
     }
 }
