148fb78e94fd6dc07f2ef6e515e50f07dbf99bf1
Allun <ocarina51006@gmail.com>
2023-08-28 22:33:43 +0800

    首頁登出功能完成

---
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Controllers/Home/MemberLogoutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using prjCatChaOnlineShop.Models.CDictionary;
+
+namespace prjCatChaOnlineShop.Controllers.Home
+{
+
+    public class MemberLogoutController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove(CDictionary.SK_LOINGED_USER); // 清除特定的 Session 變數
+            return RedirectToAction("Index", "Index"); // 導向登入頁面或其他適當的地方
+        }
+    }
+}
