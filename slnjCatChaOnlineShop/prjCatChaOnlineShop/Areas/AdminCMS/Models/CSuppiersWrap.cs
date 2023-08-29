eac8f72b00924f6bd043c43fabea0865c33396f9
ZONG HAN JI <numbandfaint123@gmail.com>
2023-08-29 12:19:08 +0800

    no message

---
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Areas/AdminCMS/Models/CSuppiersWrap.cs
@@ -0,0 +1,12 @@
+﻿namespace prjCatChaOnlineShop.Areas.AdminCMS.Models
+{
+    public class CSuppiersWrap
+    {
+
+        public string CompanyName { get; set; }
+
+        public string ContactPhone { get; set; }
+
+        public string CompanyAddress { get; set; }
+    }
+}
