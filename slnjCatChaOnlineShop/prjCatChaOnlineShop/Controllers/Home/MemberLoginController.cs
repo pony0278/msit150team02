d3f7a0a350a78f2c67684e896bcae8575a6e6afc
Allun <ocarina51006@gmail.com>
2023-09-03 16:36:17 +0800

    新增: 註冊會員驗證信功能

--- slnjCatChaOnlineShop/prjCatChaOnlineShop/Controllers/Home/MemberLoginController.cs
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Controllers/Home/MemberLoginController.cs
@@ -36,7 +36,7 @@ namespace prjCatChaOnlineShop.Controllers.Home
         [HttpPost]
         public IActionResult Login(CLoginModel vm)
         {
-            ShopMemberInfo existUser = (new cachaContext()).ShopMemberInfo.FirstOrDefault(
+            ShopMemberInfo existUser = (new cachaContext()).ShopMemberInfo.OrderBy(x => x.MemberId).LastOrDefault(
                t => t.Email.Equals(vm.txtEmail));
 
             if (existUser == null)  //帳號不存在 => 此帳號未註冊
@@ -86,8 +86,170 @@ namespace prjCatChaOnlineShop.Controllers.Home
         [HttpPost]
         public JsonResult CheckEmailExist(string email)
         {
-            bool emailExist = _context.ShopMemberInfo.Any(x => x.Email == email);
+            bool emailExist = _context.ShopMemberInfo.Any(x => x.Email == email && x.EmailVerified == true); //帳號存在而且被驗證過
             return Json(emailExist);
+        }
+
+
+        public IActionResult SendMailTokenForRegister([FromBody] SendMailTokenIn inModel)
+        {
+            SendMailTokenOut outModel = new SendMailTokenOut();
+
+            // 檢查輸入來源
+            if (string.IsNullOrEmpty(inModel.MemberID))
+            {
+                outModel.ErrMsg = "請輸入帳號";
+                return Json(outModel);
+            }
+
+            // 使用 LINQ 查詢資料庫以獲取會員資料
+            var member = _context.ShopMemberInfo
+                .Where(m => m.Email == inModel.MemberID)
+                .FirstOrDefault();
+
+            if (member != null && member.EmailVerified == false)//如果會員存在但未完成驗證
+            {
+                // 生成一個新的隨機金鑰
+                string randomKey = CKeyGenerator.GenerateRandomKey();
+                // 將隨機金鑰設置到 IConfiguration 裡
+                _configuration["VerifyEmail:SecretKey"] = randomKey;
+
+                //把使用者帳號寫進session
+                HttpContext.Session.SetString("ResetPwdUserEmail", inModel.MemberID);
+                string storedValue = HttpContext.Session.GetString("ResetPwdUserEmail");
+
+                string UserEmail = member.Email;
+
+                // 產生帳號+時間驗證碼
+                string sVerify = inModel.MemberID + "|" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+
+                // 使用AES加密
+                string encryptedVerify = EncryptString(sVerify, _configuration["VerifyEmail:SecretKey"]);
+
+                // 網站網址
+                string webPath = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}{HttpContext.Request.PathBase}";
+
+                // 從信件連結回到重設密碼頁面
+                string receivePage = "MemberLogin/VerifyEmail";
+
+                // 信件內容範本
+                string mailContent = "請點擊以下連結，以完成身分驗證。 30 分鐘後，此連結將會失效。<br><br>";
+                mailContent = mailContent + $"<a href='{webPath}/{receivePage}?verify={encryptedVerify}' target='_blank'>點此連結</a>";
+
+                // 信件主題
+                string mailSubject = "[測試] 貓抓抓Catcha 會員註冊驗證";
+
+                // Google 發信帳號密碼
+                string GoogleMailUserID = _configuration["ForgetPassword:GoogleMailUserID"];
+                string GoogleMailUserPwd = _configuration["ForgetPassword:GoogleMailUserPwd"];
+
+                // 使用 Google Mail Server 發信
+                string SmtpServer = "smtp.gmail.com";
+                int SmtpPort = 587;
+                MailMessage mms = new MailMessage();
+                mms.From = new MailAddress(GoogleMailUserID);
+                mms.Subject = mailSubject;
+                mms.Body = mailContent;
+                mms.IsBodyHtml = true;
+                mms.SubjectEncoding = Encoding.UTF8;
+                mms.To.Add(new MailAddress(UserEmail));
+
+                using (SmtpClient client = new SmtpClient(SmtpServer, SmtpPort))
+                {
+                    client.EnableSsl = true;
+                    client.Credentials = new NetworkCredential(GoogleMailUserID, GoogleMailUserPwd);
+                    client.Send(mms); //寄出信件
+                }
+
+                outModel.ResultMsg = "請於 30 分鐘內至你的信箱點擊連結完成驗證，逾期將無效";
+            }
+            else
+            {
+                outModel.ErrMsg = "查無此帳號";
+            }
+
+            return Json(outModel);
+        }
+
+
+        public IActionResult VerifyEmail(string verify) 
+        {
+            // 取得系統自定密鑰，這裡使用 IConfiguration 讀取 
+            string secretKey = _configuration["VerifyEmail:SecretKey"];
+            verify = verify.Replace(" ", "+");
+            if (string.IsNullOrEmpty(verify))
+            {
+                ViewData["ErrorMsg"] = "缺少驗證碼";
+                return View();
+            }
+            try
+            {
+                // 使用 AES 解密驗證碼
+                string decryptedVerify = DecryptString(verify, secretKey);
+
+                if (string.IsNullOrEmpty(decryptedVerify))
+                {
+                    ViewData["ErrorMsg"] = "驗證碼錯誤";
+                    return View();
+                }
+
+                // 解析分隔的資料
+                string[] verifyParts = decryptedVerify.Split('|');
+
+                if (verifyParts.Length != 2)
+                {
+                    ViewData["ErrorMsg"] = "驗證碼格式不正確";
+                    return View();
+                }
+
+                string userID = verifyParts[0];
+                DateTime resetTime;
+
+                if (!DateTime.TryParse(verifyParts[1], out resetTime))
+                {
+                    ViewData["ErrorMsg"] = "無效的重設時間";
+                    return View();
+                }
+
+                // 檢查時間是否超過 30 分鐘
+                TimeSpan timeElapsed = DateTime.Now - resetTime;
+
+                if (timeElapsed.TotalMinutes > 30)
+                {
+                    ViewData["ErrorMsg"] = "超過驗證碼有效時間，請重寄驗證碼";
+                    return View();
+                }
+
+                // 驗證碼檢查成功，加入 Session
+                HttpContext.Session.SetString("VerifyUserId", userID);
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewData["ErrorMsg"] = "處理驗證碼時出錯";
+                return View();
+            }
+
+            return RedirectToAction("Login", "MemberLogin");
+        }
+       
+
+
+        [HttpPost]
+        public IActionResult VerifyEmail() {
+
+            var User = HttpContext.Session.GetString("VerifyUserId");
+            var newMember = _context.ShopMemberInfo.OrderBy(x => x.MemberId).LastOrDefault(m => m.Email == User);
+            if (newMember != null)
+            {
+                newMember.EmailVerified = true;
+                _context.Update(newMember);
+                _context.SaveChanges();
+                HttpContext.Session.Remove("VerifyUserId");
+
+            }
+            return View();
         }
         #endregion
 
