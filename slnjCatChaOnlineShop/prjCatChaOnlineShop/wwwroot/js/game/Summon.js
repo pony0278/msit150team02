a85591046d6e00a4e910f29bd1cbf57facb75172
yaolin <peter19971115@gmail.com>
2023-09-12 16:57:05 +0800

    修正轉蛋機率調整動畫結果

--- slnjCatChaOnlineShop/prjCatChaOnlineShop/wwwroot/js/game/Summon.js
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/wwwroot/js/game/Summon.js
@@ -104,12 +104,12 @@ function showGachaResult(scaledProbability, allImages, allItemName) {
     let animationLevel = '';
 
     // 檢查是否有 SS 等級的獎項，如果有則設置動畫等級為 'SS'
-    if (scaledProbability <=3) {
+    if (scaledProbability <=1) {
         animationLevel = 'SS';
-    } else if (scaledProbability<= 6) {
-        animationLevel = 'S';
-    } else if (scaledProbability <= 15) {
+    } else if (scaledProbability<= 4) {
         animationLevel = 'CATS';
+    } else if (scaledProbability <= 8) {
+        animationLevel = 'S';
     } else {
         animationLevel = 'A';
     }
