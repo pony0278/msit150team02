960c9718bcc7e26a25e289f72cb169f813c754a6
April <smilehsiang@gmail.com>
2023-09-12 16:53:55 +0800

    ※修復刪除購物車有子項目的商品時須符合c.id和c.子項目的選項

--- slnjCatChaOnlineShop/prjCatChaOnlineShop/Controllers/Api/CartController.cs
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Controllers/Api/CartController.cs
@@ -18,12 +18,12 @@ namespace prjCatChaOnlineShop.Controllers.Api
 
 
         // 從session裡面刪除購物車商品
-        public IActionResult DeleteCartItem(int? id)
+        public IActionResult DeleteCartItem(int? id,string 子項目)
         {
             // 檢查傳入的 id 是否有效
-            if (id == null)
+            if (id == null || string.IsNullOrEmpty(子項目))
             {
-                return BadRequest("無效的商品 ID");
+                return BadRequest("無效的商品 ID 或子項目");
             }
 
             // 從 Session 中獲取購物車內容
@@ -34,9 +34,8 @@ namespace prjCatChaOnlineShop.Controllers.Api
             cart = JsonSerializer.Deserialize<List<CCartItem>>(json);
             if (cart != null)
             {
-
-                // 找到具有相同 ID 的商品
-                var itemToRemove = cart.FirstOrDefault(item => item.cId == id);
+                // 找到具有相同 ID 和子項目的商品
+                var itemToRemove = cart.FirstOrDefault(item => item.cId == id && item.c子項目.Trim().Equals(子項目.Trim(), StringComparison.OrdinalIgnoreCase));
                 if (itemToRemove != null)
                 {
                     // 從購物車刪除找到的商品
@@ -57,13 +56,57 @@ namespace prjCatChaOnlineShop.Controllers.Api
                 }
                 else
                 {
-                    return BadRequest("找不到具有指定 ID 的商品");
+                    return BadRequest("找不到具有指定 ID 和子項目的商品");
                 }
             }
             else
             {
                 return BadRequest("購物車是空的");
             }
+            //// 檢查傳入的 id 是否有效
+            //if (id == null)
+            //{
+            //    return BadRequest("無效的商品 ID");
+            //}
+
+            //// 從 Session 中獲取購物車內容
+            //string json = "";
+            //List<CCartItem> cart;
+            //json = HttpContext.Session.GetString(CDictionary.SK_PURCHASED_PRODUCTS_LIST);
+            //// 反序列化 Session 中的購物車內容
+            //cart = JsonSerializer.Deserialize<List<CCartItem>>(json);
+            //if (cart != null)
+            //{
+
+            //    // 找到具有相同 ID 的商品
+            //    var itemToRemove = cart.FirstOrDefault(item => item.cId == id);
+            //    if (itemToRemove != null)
+            //    {
+            //        // 從購物車刪除找到的商品
+            //        cart.Remove(itemToRemove);
+
+            //        // 更新Session中的購物車內容
+            //        _httpContextAccessor.HttpContext.Session.SetString(CDictionary.SK_PURCHASED_PRODUCTS_LIST, JsonSerializer.Serialize(cart));
+
+            //        decimal total = (decimal)cart.Sum(item => item.c小計);
+
+            //        //創建一個匿名對象，包含商品以從購物車刪除的消息以及更新後的總金額
+            //        var response = new
+            //        {
+            //            Message = "商品已從購物車删除",
+            //            Total = total
+            //        };
+            //        return new JsonResult(response);
+            //    }
+            //    else
+            //    {
+            //        return BadRequest("找不到具有指定 ID 的商品");
+            //    }
+            //}
+            //else
+            //{
+            //    return BadRequest("購物車是空的");
+            //}
         }
     }
 }
